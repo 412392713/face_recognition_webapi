a9db35f7f7f3a865368a71932a1d3245bc927c90
412392713 <412392713@qq.com>
2019-08-23 16:12:56 +0800

    Signed-off-by: 412392713 <412392713@qq.com>

--- face_match.py
+++ face_match.py
@@ -44,9 +44,9 @@ def detect_faces_in_image(file_stream1, file_stream2):
     is_match = False
 
     if len(unknown_face_encodings) > 0:
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0], 0.4)
+        match_results = face_recognition.compare_faces(known_face_encoding, unknown_face_encodings[0], 0.4)
         #print(jsonify(match_results))
-        if match_results[0][0]:
+        if match_results[0]:
             is_match = True
 
     # Return the result as json
