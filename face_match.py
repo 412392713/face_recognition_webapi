788005282a8736243739677758af3801452b55ca
412392713 <412392713@qq.com>
2019-08-24 08:23:08 +0800

    Signed-off-by: 412392713 <412392713@qq.com>

--- face_match.py
+++ face_match.py
@@ -1,42 +1,44 @@
 # -*- coding: utf-8 -*-
 import face_recognition
 from flask import Flask, jsonify, request, redirect
-
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+from face_util import allowed_file
+import os
 
 
 def face_match():
     if request.method == 'POST':
-        if 'img1' not in request.files or 'img2' not in request.files:
-            return redirect(request.url)
-
         img1 = request.files['img1']
         img2 = request.files['img2']
 
-        if img1.filename == '' or img2.filename == '' :
-            return redirect(request.url)
-
-        if img1 and img2 and allowed_file(img1.filename) and allowed_file(img2.filename):
-            return detect_faces_in_image(img1, img2, request.values.get("tolerance"))
+        if img1 and img2 and allowed_file(img1) and allowed_file(img2):
+            file_path1 = os.path.join(os.getcwd(),img1)
+            file_path2 = os.path.join(os.getcwd(),img2)
+            return detect_faces_in_image(file_path1, file_path2, request.values.get("tolerance"))
 
     return '''
     <!doctype html>
     <title>人像匹配</title>
     <h1>人像匹配</h1>
     <form method="POST" enctype="multipart/form-data">
-      <input type="file" name="img1">
-	  <input type="file" name="img2">
+      图片路径1：<input type="text" name="img1">
+	  图片路径2：<input type="text" name="img2">
       <br/>
-      参数：越小越严格
+      ----------参数：越小越严格---------------
       <br/>
 	  <input type="text" name="tolerance" value="0.4">
       <br/>
       <input type="submit" value="提交">
+      <br/>
+      <br/>
+      <br/>
+      --------------说明----------------------------
+      返回：
+      {
+        "is_match": 是否匹配(true/false)
+      }
+      <br/>
+      <br/>
+      <br/>
     </form>
     '''
 
