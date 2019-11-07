8a340e2cecad1f7eba04b6cfaa96ee3018bee5c5
412392713 <412392713@qq.com>
2019-11-07 11:41:47 +0800

    Signed-off-by: 412392713 <412392713@qq.com>

--- pp_mrz.py
+++ pp_mrz.py
@@ -28,10 +28,7 @@ def pp_mrz():
       <br/>
       --------------说明----------------------------
       <br/>
-      返回：
-      {
-        "mrz": 护照MRZ
-      }
+      返回：{"mrz":{"check_composite":"8","check_date_of_birth":"3","check_expiration_date":"0","check_number":"7","check_personal_number":"2","country":"KOR","date_of_birth":"991129","expiration_date":"230430","method":"direct","mrz_type":"TD3","names":"JAEWONK","nationality":"KOR","number":"M87540270","personal_number":"1057515V171787","sex":"M","surname":"BANG","type":"PM","valid_composite":true,"valid_date_of_birth":true,"valid_expiration_date":true,"valid_number":true,"valid_personal_number":true,"valid_score":100}}
       <br/>
       <br/>
       <br/>
